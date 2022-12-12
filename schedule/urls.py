7901d508181b094dfc27d677161a4ac0850a922d
sei-66 <seina623@icloud.com>
2022-12-13 00:11:58 +0900

    Add:Schedule_Create,Schedule_Delete

--- schedule/urls.py
+++ schedule/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    path("schedule/", views.ListScheduleView.as_view()),
-    path("schedule/<int:pk>/detail", views.DetailScheduleView.as_view()),
+    path("schedule/", views.ListScheduleView.as_view(), name="list-schedule"),
+    path("schedule/<int:pk>/detail", views.DetailScheduleView.as_view(), name="detail-schedule"),
+    path("schedule/create", views.CreateScheduleView.as_view(), name="create-schedule"),
+    path("schedule/<int:pk>/delete", views.DeleteScheduleView.as_view(), name="delete-schedule"),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import Company
 
 
@@ -11,3 +12,14 @@ class ListScheduleView(ListView):
 class DetailScheduleView(DetailView):
     template_name = "schedule/schedule_detail.html"
     model = Company
+    
+class CreateScheduleView(CreateView):
+    template_name = "schedule/schedule_create.html"
+    model = Company
+    fields = ("company_name", "business", "memo", "hp_url", "rikunabi_url", "mainabi_url")
+    success_url = reverse_lazy("list-schedule")
+    
+class DeleteScheduleView(DeleteView):
+    template_name = "schedule/schedule_delete.html"
+    model = Company
+    success_url = reverse_lazy("list-schedule")
